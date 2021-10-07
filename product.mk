1a49129d8f4c2b427be7c0a0d725dcfc4f8b0237
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-10-07 12:53:56 -0400

    extra: Set Google Sans Text as the default body font.
    * With Android 12, Google Sans is now a variable font type as in
      the font (.ttf) file now contains various different font glyphs
      that the system can use when it requests for a particular weight
      and OPSZ axis values. Google Sans Text - Google's new Roboto body
      font companion is also bundled in this font file.
    
    * To make use of the different glyphs stored in the font file we
      define the OPSZ and weight of the fonts to inline with what Pixels
      have thus, giving us the same look and feel.
    
    * Change the framework font overlay to reflect the new font alias we
      created in `font_customization.xml`
    
    Change-Id: I48a747c88a23e1b8c8e20b762a6f403b8d6c4637
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/5749
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- product.mk
+++ product.mk
@@ -2,6 +2,11 @@
 PRODUCT_PACKAGES += \
     LatinIMEGooglePrebuilt
 
+## Fonts
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/fonts/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    $(LOCAL_PATH)/prebuilt/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
 # GMS
 ifeq ($(WITH_GMS), true)
 $(call inherit-product, vendor/gms/common/common-vendor.mk)
@@ -9,6 +14,9 @@ DEVICE := $(LINEAGE_BUILD)_gms
 else
 DEVICE := $(LINEAGE_BUILD)
 endif
+
+## Overlays
+PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Signing
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
