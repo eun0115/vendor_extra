f4b5189c51cbb403537615a6c62cede93b6021f7
SamarV-121 <samarvispute121@pm.me>
2022-09-14 12:55:38 +0530

    product: Include gms
    https://github.com/AOSP-XIII/vendor_gms
    
    Change-Id: Ifc4b49b54ed4b0716dbf9ee1eab78b008078e2dd

--- product.mk
+++ product.mk
@@ -1,2 +1,10 @@
+# GMS
+ifeq ($(WITH_GMS), true)
+$(call inherit-product, vendor/gms/common/common-vendor.mk)
+DEVICE := $(LINEAGE_BUILD)_gms
+else
+DEVICE := $(LINEAGE_BUILD)
+endif
+
 # Signing
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
